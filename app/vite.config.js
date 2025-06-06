8a419d3cf96ceaac2b190c137478a43a2a6c079c
sisyohus-team <data@eratani.co.id>
2025-06-07 00:09:54 +0700

    fix: fix github pages deployment

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,15 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/heyzik/', // Add this line - should match your repository name
+  plugins: [vue()],
+  base: '/heyzik/', // This should match your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
